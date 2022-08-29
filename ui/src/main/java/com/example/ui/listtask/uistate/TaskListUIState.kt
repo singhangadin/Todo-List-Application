bab4febf9c3d7d6bbf26ef6cf5b429d7eda64668
Angad Singh <angads25@gmail.com>
2022-08-30 00:18:57 +0530

    Completed List Task UI State

--- ui/src/main/java/com/example/ui/listtask/uistate/TaskListUIState.kt
+++ ui/src/main/java/com/example/ui/listtask/uistate/TaskListUIState.kt
@@ -2,4 +2,12 @@ package com.example.ui.listtask.uistate
 
 sealed class TaskListUIState {
 
+    object ShowLoader: TaskListUIState()
+    object HideLoader: TaskListUIState()
+
+    object ShowEmptyView: TaskListUIState()
+    object HideEmptyView: TaskListUIState()
+
+    class ShowMessage(val message: Int): TaskListUIState()
+    class ShowSaveTaskScreen(val taskId: String?): TaskListUIState()
 }
