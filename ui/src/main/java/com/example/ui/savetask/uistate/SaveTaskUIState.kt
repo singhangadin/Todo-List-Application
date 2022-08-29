a8cb7237df67b7be8ad8b319290f8a8dac8f155b
Angad Singh <angads25@gmail.com>
2022-08-30 00:29:35 +0530

    Completed Save Task UI State

--- ui/src/main/java/com/example/ui/savetask/uistate/SaveTaskUIState.kt
+++ ui/src/main/java/com/example/ui/savetask/uistate/SaveTaskUIState.kt
@@ -1,4 +1,11 @@
 package com.example.ui.savetask.uistate
 
 sealed class SaveTaskUIState {
+    object ShowLoader: SaveTaskUIState()
+    object HideLoader: SaveTaskUIState()
+
+    class ShowMessage(val message: Int): SaveTaskUIState()
+
+    object Success: SaveTaskUIState()
+    object ShowDatePicker : SaveTaskUIState()
 }
