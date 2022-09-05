aa89b31423d1b6da704fbe54afac1dc33f42d9df
Angad Singh <angads25@gmail.com>
2022-09-06 01:42:07 +0530

    Introduced compose module

---
+++ compose-ui/src/main/java/in/singhangad/compose_ui/listtask/TaskListScreen.kt
@@ -0,0 +1,14 @@
+package `in`.singhangad.compose_ui.listtask
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TaskListScreen() {
+}
+
+@Preview
+@Composable
+fun showPreview(){
+    TaskListScreen()
+}
---
+++ compose-ui/src/main/java/in/singhangad/compose_ui/savetask/SaveTaskScreen.kt
@@ -0,0 +1,14 @@
+package `in`.singhangad.compose_ui.savetask
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveTaskScreen() {
+}
+
+@Preview
+@Composable
+fun showPreview(){
+    SaveTaskScreen()
+}
