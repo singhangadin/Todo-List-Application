8f98628060e332d77572da15fab0203a0a0cfb57
Angad Singh <angads25@gmail.com>
2022-09-07 02:07:01 +0530

    Implemented composable UI of save screen

--- ui-compose/src/main/java/in/singhangad/compose_ui/savetask/SaveTaskScreen.kt
+++ ui-compose/src/main/java/in/singhangad/compose_ui/savetask/SaveTaskScreen.kt
@@ -1,33 +1,106 @@
 package `in`.singhangad.compose_ui.savetask
 
 import `in`.singhangad.compose_ui.R
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import `in`.singhangad.compose_ui.values.TextTitleStyle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SaveTaskScreen() {
     MaterialTheme {
         Scaffold(
             topBar = {
-                TopAppBar {  }
+                TopAppBar {
+                    Text(text = stringResource(id = R.string.label_task_doc))
+                }
             }
-        ) { contentPadding ->
-            Column(modifier = Modifier.padding(contentPadding)) {
-                Text(text = stringResource(R.string.label_task_title))
-                TextField(value = TextFieldValue(), onValueChange = {
+        ) {
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(
+                            dimensionResource(id = R.dimen.margin_regular)
+                        )
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                ) {
+                    var titleText by remember { mutableStateOf("") }
+                    var descriptionText by remember { mutableStateOf("") }
 
-                })
-                Text(text = stringResource(R.string.label_task_description))
-                TextField(value = TextFieldValue(), onValueChange = {
+                    Text(
+                        text = stringResource(R.string.label_task_title),
+                        style = TextTitleStyle
+                    )
+                    val horizontalScrollState = rememberScrollState(0)
+                    BasicTextField(
+                        value = titleText,
+                        singleLine = true,
+                        onValueChange = { titleText = it },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp)
+                            .horizontalScroll(horizontalScrollState),
+                        textStyle = TextStyle.Default.copy(fontSize = 16.sp)
+                    )
+                    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_small)))
+                    Text(
+                        text = stringResource(R.string.label_task_description),
+                        style = TextTitleStyle
+                    )
+                    val verticalScrollState = rememberScrollState(0)
+                    BasicTextField(
+                        value = descriptionText,
+                        onValueChange = {
+                            descriptionText = it
+                        },
+                        maxLines = 4,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp)
+                            .verticalScroll(verticalScrollState),
+                        textStyle = TextStyle.Default.copy(fontSize = 16.sp)
+                    )
+                    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.margin_small)))
+                    Text(
+                        text = stringResource(R.string.label_task_doc),
+                        style = TextTitleStyle
+                    )
+                    Text(
+                        text = stringResource(R.string.label_hint_date),
+                        color = Color.Gray
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
+                        Text(text = stringResource(id = R.string.label_task_save))
+                    }
+                }
 
-                })
-                Text(text = stringResource(R.string.label_task_doc))
+//                CircularProgressIndicator(
+//                    Modifier.align(Alignment.Center)
+//                )
             }
         }
     }
---
+++ ui-compose/src/main/java/in/singhangad/compose_ui/values/Themes.kt
@@ -0,0 +1,9 @@
+package `in`.singhangad.compose_ui.values
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
+val TextTitleStyle = TextStyle(
+    fontSize = TitleTextSize,
+    fontWeight = FontWeight.Bold
+)
