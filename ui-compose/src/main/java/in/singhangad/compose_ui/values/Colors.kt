6287795c4df5f84326057780c7d0253adde8d22c
Angad Singh <angads25@gmail.com>
2022-09-21 22:56:53 +0530

    Added color theme

---
+++ ui-compose/src/main/java/in/singhangad/compose_ui/values/Colors.kt
@@ -0,0 +1,27 @@
+package `in`.singhangad.compose_ui.values
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF8307F0)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_colorPrimary = Color(0xFF8307F0)
+val md_theme_light_colorPrimaryVariant = Color(0xFF3700B3)
+val md_theme_light_colorOnPrimary = Color(0xFFFFFFFF)
+
+val md_theme_light_colorSecondary = Color(0xFF03DAC5)
+val md_theme_light_colorSecondaryVariant = Color(0xFF018786)
+val md_theme_light_colorOnSecondary = Color(0xFF000000)
+
+val md_theme_light_statusBarColor = md_theme_light_colorPrimaryVariant
+
+val md_theme_dark_primary = Color(0xFFD9B9FF)
+val md_theme_dark_onPrimary = Color(0xFF450085)
+val md_theme_dark_colorPrimary = Color(0xFFBB86FC)
+val md_theme_dark_colorPrimaryVariant = Color(0xFF3700B3)
+val md_theme_dark_colorOnPrimary = Color(0xFF000000)
+
+val md_theme_dark_colorSecondary = Color(0xFF03DAC5)
+val md_theme_dark_colorSecondaryVariant = Color(0xFF03DAC5)
+val md_theme_dark_colorOnSecondary = Color(0xFF000000)
+
+val md_theme_dark_statusBarColor = md_theme_dark_colorPrimaryVariant
