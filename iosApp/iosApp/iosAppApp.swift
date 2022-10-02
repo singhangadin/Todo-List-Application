f18d3a6c528a283343014044df5328892f8170f9
Angad Singh <angads25@gmail.com>
2022-10-02 23:43:14 +0530

    Added Ios App

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Angad Singh on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
